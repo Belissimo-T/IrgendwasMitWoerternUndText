3f8744a84758e06bec7770eabc07b5866f0522f2
Bela <bela.tollking@gmail.com>
2021-06-21 18:37:49 +0200

    yes

---
+++ discord_bot.py
@@ -0,0 +1,111 @@
+import discord
+import main
+from io import BytesIO
+import traceback
+import sys
+
+client = discord.Client()
+
+wb_usage = discord.Embed(title="Usage of `!wörterbuch`",
+                         description="Usage: `!wörterbuch '[word]' '[ipa]' '[part_of_speech]' "
+                                     "'[meaning]' '[example]' <zoom>`\n\nExample: ```!wörterbuch \"rein·joi·nen\" "
+                                     "\"ˈraɪndʒɔɪnən\" \"Verb\" \"einen Internetanruf oder eine Videospielsession "
+                                     "betreten\" '\"Ahh! Er ist wieder reingejoined.\"'```",
+                         color=discord.Color(0xFFFF00))
+
+wb_usage.add_field(name="word", value="The word. This symbol might be helpful: `·`")
+wb_usage.add_field(name="ipa", value="The phonetic transcription of the word. Mark the stressed syllable with a `ˈ`.")
+wb_usage.add_field(name="part_of_speech",
+                   value="The part of speech of the word. E.g. `Substantiv`, `Verb`, `Adjektiv`.")
+wb_usage.add_field(name="meaning", value="The meaning of the word.")
+wb_usage.add_field(name="example", value="An example usage of the word. Contain inside double quotes "
+                                         "(`'\"example\"'`).")
+wb_usage.add_field(name="zoom", value="Optional. Can be any float < `5` (I think). "
+                                      "Specifies the magnification factor. Default is `3`.")
+
+wb_usage.set_footer(text="There is also a !g2p (grapheme to phoneme) command that helps getting the ipa strings.")
+
+g2p_usage = discord.Embed(title="Usage of `!g2p`",
+                          description="Usage: `!g2p '[word]' '[lang]'`\n\n"
+                                      "Example: ```!g2p 'join' 'eng-US'```",
+                          color=discord.Color(0xFFFF00))
+
+g2p_usage.add_field(name="word", value="The word that you wish to convert to ipa.")
+g2p_usage.add_field(name="lang", value="The language of the word. E.g. `deu`, `eng-US`. Almost every three-letter code "
+                                       "works. If in doubt, look it up in the [api specification]"
+                                       "(http://clarin.phonetik.uni-muenchen.de/BASWebServices/services/help) under "
+                                       "`runG2P`.")
+g2p_usage.add_field(name="**IMPORTANT NOTICE**",
+                    value="This calls the [BAS API]"
+                          "(https://clarin.phonetik.uni-muenchen.de/BASWebServices/interface/Grapheme2Phoneme) whose "
+                          "Terms Of Usage can be found [here]"
+                          "(https://clarin.phonetik.uni-muenchen.de/BASWebServices/help/termsOfUsage#termsofusage). "
+                          "It states that the usage of this API is for **academic (non-profit research) use only** and "
+                          "the user **must be part of an academic institution**.\nThis means: **Do not spam** and "
+                          "**don't give away any private information**.", inline=False)
+g2p_usage.set_footer(text="Also have a look at the !wörterbuch command.")
+
+def construct_error_embed(err: str):
+    return discord.Embed(title="Error", description=f"Oh snap! Something went wrong:\n```{err}```",
+                         color=discord.Color(0xFF0000))
+
+
+def parse_args(message: str):
+    args = []
+    start = 0
+    for i in range(len(message)):
+        try:
+            arg = eval(message[start:i + 1])
+            args.append(arg)
+            start = i + 1
+        except Exception:
+            ...
+    return args
+
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+
+@client.event
+async def on_message(message: discord.Message):
+    try:
+        if message.content.startswith("!wörterbuch"):
+            # message.channel: discord.TextChannel
+
+            args = parse_args(message.content[12:])
+            if 5 > len(args) or len(args) > 6:
+                # message.channel: discord.TextChannel
+                await message.channel.send(embed=wb_usage, reference=message)
+            else:
+                try:
+                    stream = BytesIO(main.get_image(*args))
+                    # message.channel: discord.TextChannel
+                    # message.author: discord.User
+                    word = args[0].replace("·", "")
+                    await message.channel.send(f"{word} von {message.author.mention}",
+                                               file=discord.File(stream, filename=f"{word.replace(' ', '_')}.png"),
+                                               reference=message)
+                except Exception:
+                    err = traceback.format_exc()
+                    sys.stderr.write(err)
+                    await message.channel.send(embed=construct_error_embed(err), reference=message)
+        elif message.content.startswith("!g2p"):
+            args = parse_args(message.content[4:])
+
+            if len(args) != 2:
+                await message.channel.send(embed=g2p_usage, reference=message)
+            else:
+                raise NotImplementedError()
+
+    except Exception:
+        err = traceback.format_exc()
+        sys.stderr.write(err)
+        await message.channel.send(embed=construct_error_embed(err))
+
+
+with open("secret.token", "r") as f:
+    TOKEN = f.read()
+
+client.run(TOKEN)
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import io
+
 from PIL import Image, PngImagePlugin
 from io import BytesIO
 import selenium.common.exceptions
@@ -10,20 +12,6 @@ chrome_options = Options()
 chrome_options.add_argument("--headless")
 
 # · ˈ
-word = "putt"
-ipa = "ˈpʊt"
-part_of_speech = "Adjektiv"
-meaning = "kaputt"
-example = '"Meine Spitzhacke ist gerade putt gegangen."'
-
-zoom = 3
-pad = 5 * zoom
-
-xpaths_changes = {"div/div[2]/div[1]/div/span": word,
-                  "div/div[2]/div[2]/span/span": ipa,
-                  "div/div[4]/div/div/div/div/div/i/span": part_of_speech,
-                  "div/div[4]/div/div/ol/li[1]/div/div/div[1]/div[2]/div/div[1]/span": meaning,
-                  "div/div[4]/div/div/ol/li[1]/div/div/div[1]/div[2]/div/div[2]/div": example}
 
 print("Creating browser...")
 driver = webdriver.Chrome("./chromedriver", options=chrome_options)
@@ -38,61 +26,88 @@ i_agree.click()
 print("Find the base dictionary element...")
 frame = driver.find_element_by_class_name("lr_container").find_elements_by_xpath("./*")[2]
 
-print("Changing...")
-for xpath in xpaths_changes:
-    change = xpaths_changes[xpath]
-    print(f"{xpath} to {change}")
-    try:
-        element = frame.find_element_by_xpath(xpath)
-        # change text of element
-        driver.execute_script(f"arguments[0].innerText = '{change}'", element)
-    except selenium.common.exceptions.NoSuchElementException:
-        print(":((")
 
-print("Resizing window...")
-driver.set_window_size(1600 * zoom, 900 * zoom)
+def get_image(word, ipa, part_of_speech, meaning, example, zoom=3):
+    pad = 5 * zoom
 
-print("Scaling window...")
-driver.execute_script(f"document.body.style.zoom='{zoom}'")
+    xpaths_changes = {"div/div[2]/div[1]/div/span": word,
+                      "div/div[2]/div[2]/span/span": ipa,
+                      "div/div[4]/div/div/div/div/div/i/span": part_of_speech,
+                      "div/div[4]/div/div/ol/li[1]/div/div/div[1]/div[2]/div/div[1]/span": meaning,
+                      "div/div[4]/div/div/ol/li[1]/div/div/div[1]/div[2]/div/div[2]/div": example}
 
-print("Scrolling frame into view...")
-ActionChains(driver).move_to_element(frame).perform()
-# driver.execute_script("arguments[0].scrollIntoView(true);", frame)
+    print("Changing...")
+    for xpath in xpaths_changes:
+        change = xpaths_changes[xpath]
+        print(f"{xpath} to {change}")
+        try:
+            element = frame.find_element_by_xpath(xpath)
+            # change text of element
+            driver.execute_script(f"arguments[0].innerText = '{change}'", element)
+        except selenium.common.exceptions.NoSuchElementException:
+            print(":((")
 
-print("Taking screenshot...")
-location = frame.location
-size = frame.size
-print("...Getting png")
-png = driver.get_screenshot_as_png()  # saves screenshot of entire page
+    print("Resizing window...")
+    driver.set_window_size(1600 * zoom, 900 * zoom)
 
-print("...Opening with PIL")
-im: PngImagePlugin.PngImageFile = Image.open(BytesIO(png))  # uses PIL library to open image in memory
+    print("Scaling window...")
+    driver.execute_script(f"document.body.style.zoom='{zoom}'")
 
-left = location['x'] * zoom - pad  # must mutliply all these numbers by your zoom
-top = location['y'] * zoom - pad
-right = (location['x'] + size['width']) * zoom + pad
-bottom = (location['y'] + size['height']) * zoom + pad
+    print("Scrolling frame into view...")
+    ActionChains(driver).move_to_element(frame).perform()
+    # driver.execute_script("arguments[0].scrollIntoView(true);", frame)
 
-print("...Cropping")
-im = im.crop((left, top, right, bottom))
+    print("Taking screenshot...")
+    location = frame.location
+    size = frame.size
+    print("...Getting png")
+    png = driver.get_screenshot_as_png()  # saves screenshot of entire page
 
-print("...Searching for black")
-white = (255, 255, 255, 255)
-for x in range(im.width-1, -1, -1):
-    for y in range(0, im.height):
-        if im.getpixel((x, y)) != white:
-            break
+    print("...Opening with PIL")
+    im: PngImagePlugin.PngImageFile = Image.open(BytesIO(png))  # uses PIL library to open image in memory
+
+    left = location['x'] * zoom - pad  # must mutliply all these numbers by your zoom
+    top = location['y'] * zoom - pad
+    right = (location['x'] + size['width']) * zoom + pad
+    bottom = (location['y'] + size['height']) * zoom + pad
+
+    print("...Cropping")
+    im = im.crop((left, top, right, bottom))
+
+    print("...Searching for black")
+    white = (255, 255, 255, 255)
+    for x in range(im.width - 1, -1, -1):
+        for y in range(0, im.height):
+            if im.getpixel((x, y)) != white:
+                break
+        else:
+            continue
+        break
     else:
-        continue
-    break
-else:
-    raise Exception("Screenshot is blank, maybe you overdid the zoom?")
-print("...Cropping again")
-im = im.crop((0, 0, x+pad, im.height))
-print("...Saving")
-im.save(f'out.png')  # saves new cropped image
+        raise Exception("Screenshot is blank, maybe you overdid the zoom?")
 
-print("Closing browser...")
-driver.close()
+    print("...Cropping again")
+    im = im.crop((0, 0, x + pad, im.height))
 
-print("Finished!")
+    print("...Saving")
+    img_byte_arr = io.BytesIO()
+    im.save(img_byte_arr, format="png")  # saves new cropped image
+
+    print("Closing browser...")
+    # driver.close()
+
+    print("Finished!")
+
+    return img_byte_arr.getvalue()
+
+
+if __name__ == '__main__':
+    word = "putt"
+    ipa = "ˈpʊt"
+    part_of_speech = "Adjektiv"
+    meaning = "kaputt"
+    example = '"Meine Spitzhacke ist gerade putt gegangen."'
+    zoom = 3
+
+    with open("out.png", "wb") as f:
+        f.write(get_image(word, ipa, part_of_speech, meaning, example, zoom))
