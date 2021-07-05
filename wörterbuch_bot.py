e9add9eb49affb88bb5c44702b18cb7b2da542ed
Bela <bela.tollking@gmail.com>
2021-07-05 08:22:50 +0200

    minor tweaks and bugfixes, changed square brackets to <>.

--- wörterbuch_bot.py
+++ wörterbuch_bot.py
@@ -1,16 +1,14 @@
 import asyncio
 import io
 import os
+import discord
 from typing import Literal
 
-import postermywall
 import postermywall as pmw
 import wörterbuch
-from dc_bot_framework import route, run
 import g2p
+from dc_bot_framework import route, run
 from zitat import get_zitat, get_image
-
-import discord
 
 if not os.path.exists("dictionaries/global.dict"):
     dictionary = wörterbuch.Dictionary("global")
@@ -22,8 +20,8 @@ ESCAPED_CHARS = "`~_()[]*<>"
 
 def get_wb_help(name: str):
     help_embed = discord.Embed(title=f"Usage of `!wörterbuch {name}`",
-                               description="Usage: `!wörterbuch add [word] [ipa] [part_of_speech] "
-                                           f"[meaning] [example] `\n\nExample: ```!wörterbuch {name} \"rein·joi·nen\" "
+                               description="Usage: `!wörterbuch add <word> <ipa> <part_of_speech> "
+                                           f"<meaning> <example> `\n\nExample: ```!wörterbuch {name} \"rein·joi·nen\" "
                                            "\"ˈraɪndʒɔɪnən\" \"Verb\" \"einen Internetanruf oder eine "
                                            "Videospielsession betreten\" \"Ahh! Er ist wieder reingejoined.\"```",
                                color=discord.Color(0xFFFF00))
@@ -45,10 +43,15 @@ def escape(string: str) -> str:
     return "".join(["\\" * (char in ESCAPED_CHARS) + char for char in string])
 
 
-@route("!postermywall render", not_allowed_in_parallel=True)
-async def postermywall_render(message: discord.Message, id_: str, changes):
-    print("RENDER")
-    template = await postermywall.Template.from_id(id_)
+@route("!wörterbuch mastertest", only_from=311516082320048128)
+async def testest(client: discord.Client, message: discord.Message, echo: str = ""):
+    if echo:
+        await message.channel.send(echo)
+
+
+@route("!postermywall render")
+async def postermywall_render(client: discord.Client, message: discord.Message, id_: str, changes):
+    template = await pmw.Template.from_id(id_)
 
     out = discord.Embed(title=f"Custom Template based on `{id_}`", description=f"command: `!postermywall render "
                                                                                f"{id_!r} {changes!r}`")
@@ -57,13 +60,14 @@ async def postermywall_render(message: discord.Message, id_: str, changes):
     await message.channel.send(embed=out, file=file)
 
 
-@route("!postermywall attrs", not_allowed_in_parallel=True)
-async def postermywall_attrs(message: discord.Message, template_id: str):
-    await message.channel.send(embed=await (await postermywall.Template.from_id(template_id)).get_dc_attrs_embed())
+@route("!postermywall attrs")
+async def postermywall_attrs(client: discord.Client, message: discord.Message, template_id: str):
+    template = await pmw.Template.from_id(template_id)
+    await message.channel.send(embed=await template.get_dc_attrs_embed())
 
 
 @route("!postermywall search")
-async def postermywall_search(message: discord.Message, search_str: str,
+async def postermywall_search(client: discord.Client, message: discord.Message, search_str: str,
                               type_: Literal["all", "image", "video"] = "all", size: str = "all"):
     await asyncio.gather(*[message.channel.send("temporary message, gets auto-deleted after 2 min.",
                                                 embed=template.get_dc_embed(), delete_after=2 * 60,
@@ -72,9 +76,9 @@ async def postermywall_search(message: discord.Message, search_str: str,
 
 
 @route("!zitat help")
-async def zitat_help(message: discord.Message):
+async def zitat_help(client: discord.Client, message: discord.Message):
     help_embed = discord.Embed(title="Usage of `!zitat`",
-                               description="Usage: `!zitat [text] [author]`\n\nExample: "
+                               description="Usage: `!zitat <text> <author>`\n\nExample: "
                                            "```!zitat \"Trapdoors und Repeater sind eigentlich das gleiche.\" "
                                            "\"Zwakel\"```",
                                color=discord.Color(0xFFFF00))
@@ -86,7 +90,7 @@ async def zitat_help(message: discord.Message):
 
 
 @route("!zitat")
-async def zitat(message: discord.Message, text: str, author: str):
+async def zitat(client: discord.Client, message: discord.Message, text: str, author: str):
     background = get_image()
     for _ in range(5):
         img = io.BytesIO(get_zitat(text, author, background))
@@ -98,7 +102,7 @@ async def zitat(message: discord.Message, text: str, author: str):
 
 
 @route("!getmsg")
-async def getmsg(message: discord.Message, id_: str):
+async def getmsg(client: discord.Client, message: discord.Message, id_: str):
     # message.channel: discord.TextChannel
     msg: discord.Message = await message.channel.fetch_message(int(id_))
 
@@ -111,21 +115,21 @@ async def getmsg(message: discord.Message, id_: str):
     await message.channel.send(embed=out)
 
 
-@route("!wörterbuch fullhelp")
-async def fullhelp(message: discord.Message):
-    await message.channel.send("!wörterbuch render help")
-    await message.channel.send("!wörterbuch add help")
-    await message.channel.send("!wörterbuch search help")
-    await message.channel.send("!wörterbuch list help")
-    await message.channel.send("!wörterbuch remove help")
-    await message.channel.send("!wörterbuch help")
-    await message.channel.send("!wörterbuch fullhelp help")
-    await message.channel.send("!g2p help")
+# @route("!wörterbuch fullhelp")
+# async def fullhelp(message: discord.Message):
+#     await message.channel.send("!wörterbuch render help")
+#     await message.channel.send("!wörterbuch add help")
+#     await message.channel.send("!wörterbuch search help")
+#     await message.channel.send("!wörterbuch list help")
+#     await message.channel.send("!wörterbuch remove help")
+#     await message.channel.send("!wörterbuch help")
+#     await message.channel.send("!wörterbuch fullhelp help")
+#     await message.channel.send("!g2p help")
 
 
 @route("!wörterbuch help")
 @route("!wörterbuch")
-async def wb(message: discord.Message):
+async def wb(client: discord.Client, message: discord.Message):
     help_embed = discord.Embed(title="Commands of the Wörterbuch-Bot", color=discord.Color(0xFFFF00),
                                description="Tip: Add a `help` to any command to show its help.")
     help_embed.add_field(name="`!wörterbuch render`", value="Renders one Wörterbuch-entry.", inline=False)
@@ -146,39 +150,39 @@ async def wb(message: discord.Message):
 
 
 @route("!wörterbuch render help")
-async def wb_render_help(message: discord.Message):
+async def wb_render_help(client: discord.Client, message: discord.Message):
     await message.channel.send(embed=get_wb_help("render"))
 
 
 @route("!wörterbuch render")
-async def wb_render(message: discord.Message, word_, ipa, part_of_speech, meaning, example):
+async def wb_render(client: discord.Client, message: discord.Message, word_, ipa, part_of_speech, meaning, example):
     word = wörterbuch.Word(wörterbuch.split_word(word_, "·*"), ipa, part_of_speech,
                            meaning, example)
 
-    embed, file = word.get_dc_embed()
+    embed, file = await word.get_dc_embed()
     await message.channel.send(file=file, embed=embed)
 
 
 @route("!wörterbuch add help")
-async def wb_add_help(message: discord.Message):
+async def wb_add_help(client: discord.Client, message: discord.Message):
     await message.channel.send(embed=get_wb_help("add"))
 
 
 @route("!wörterbuch add")
-async def wb_add(message: discord.Message, word_, ipa, part_of_speech, meaning, example):
+async def wb_add(client: discord.Client, message: discord.Message, word_, ipa, part_of_speech, meaning, example):
     word = wörterbuch.Word(wörterbuch.split_word(word_, "·*"), ipa, part_of_speech,
                            meaning, example)
 
     dictionary.add_word(word)
 
-    embed, file = word.get_dc_embed("Added word to dictionary ✅")
+    embed, file = await word.get_dc_embed("Added word to dictionary ✅")
     await message.channel.send(file=file, embed=embed)
 
 
 @route("!wörterbuch remove help")
-async def wb_search_help(message: discord.Message):
+async def wb_search_help(client: discord.Client, message: discord.Message):
     help_embed = discord.Embed(title="Usage of `!wörterbuch search`",
-                               description="Usage: `!wörterbuch remove [word]`\n\nExample: "
+                               description="Usage: `!wörterbuch remove <word>`\n\nExample: "
                                            "```!wörterbuch remove \"reinjoinen\"```",
                                color=discord.Color(0xFFFF00))
 
@@ -191,7 +195,7 @@ async def wb_search_help(message: discord.Message):
 
 
 @route("!wörterbuch remove")
-async def wb_remove(message: discord.Message, word: str):
+async def wb_remove(client: discord.Client, message: discord.Message, word: str):
     try:
         dictionary.remove_word(word)
 
@@ -203,7 +207,7 @@ async def wb_remove(message: discord.Message, word: str):
 
 
 @route("!wörterbuch list help")
-async def wb_list_help(message: discord.Message):
+async def wb_list_help(client: discord.Client, message: discord.Message):
     help_embed = discord.Embed(title="Usage of `!wörterbuch list`",
                                description="Usage: `!wörterbuch list`\n\nExample: "
                                            "```!wörterbuch list```",
@@ -212,23 +216,25 @@ async def wb_list_help(message: discord.Message):
 
 
 @route("!wörterbuch list")
-async def wb_list(message: discord.Message):
+async def wb_list(client: discord.Client, message: discord.Message):
     await message.channel.send("temporary message, gets auto-deleted after 2 min",
                                embed=discord.Embed(title="Wörterbuch Listing",
                                                    description=f"total word count: `{len(dictionary)}`"),
                                delete_after=2 * 60)
 
     for word in dictionary:
+        word: wörterbuch.Word
         embed = discord.Embed(title=word.get_display_name())
         embed.set_image(url="attachment://image.png")
         await message.channel.send("temporary message, gets auto-deleted after 2 min", embed=embed,
-                                   file=word.get_dc_file(), delete_after=2 * 60)
+                                   file=await word.get_dc_file(),
+                                   delete_after=2 * 60)
 
 
 @route("!wörterbuch search help")
-async def wb_search_help(message: discord.Message):
+async def wb_search_help(client: discord.Client, message: discord.Message):
     help_embed = discord.Embed(title="Usage of `!wörterbuch search`",
-                               description="Usage: `!wörterbuch search [query]`\n\nExample: "
+                               description="Usage: `!wörterbuch search <query>`\n\nExample: "
                                            "```!wörterbuch search \"reinjoinen\"```",
                                color=discord.Color(0xFFFF00))
 
@@ -241,11 +247,11 @@ async def wb_search_help(message: discord.Message):
 
 
 @route("!wörterbuch search")
-async def wb_search(message: discord.Message, query: str):
+async def wb_search(client: discord.Client, message: discord.Message, query: str):
     results = dictionary.search_word(query)
 
     for i, word in enumerate(results):
-        embed, file = word.get_dc_embed(f"Search result #{i + 1}")
+        embed, file = await word.get_dc_embed(f"Search result #{i + 1}")
         await message.channel.send(file=file, embed=embed)
 
     if len(results) == 0:
@@ -255,9 +261,9 @@ async def wb_search(message: discord.Message, query: str):
 
 
 @route("!g2p help")
-async def g2p_help(message: discord.Message):
+async def g2p_help(client: discord.Client, message: discord.Message):
     help_embed = discord.Embed(title="Usage of `!g2p`",
-                               description="Usage: `!g2p [word] [lang]`\n\n"
+                               description="Usage: `!g2p <word> <lang>`\n\n"
                                            "Example: ```!g2p 'join' 'eng-US'```",
                                color=discord.Color(0xFFFF00))
 
@@ -282,7 +288,7 @@ async def g2p_help(message: discord.Message):
 
 
 @route("!g2p")
-async def g2p_(message: discord.Message, _word, lang):
+async def g2p_(client: discord.Client, message: discord.Message, _word, lang):
     phonemes = g2p.g2p(_word, lang)
 
     _, predicted_text_syllables = g2p.get_syllables(phonemes, _word)
